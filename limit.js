7f1d8649a96812fce672a3ac872efe2620acac3b
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-15 16:07:57 +0530

    limit.js

---
+++ limit.js
@@ -0,0 +1,22 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const url = 'mongodb://localhost:27017/myDB'
+
+MongoClient.connect(url, function (error, db) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Connect to', url);
+        let dbo = db.db('myDB');
+        dbo.collection("customers").find().limit(5).toArray( function (err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(res);
+                db.close().then(r => console.log('Connection is closed'));
+            }
+        })
+
+    }
+})
