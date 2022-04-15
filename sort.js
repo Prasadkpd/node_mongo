88f76a4462126589a70e667250e28a1ea241f9fb
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-15 08:27:55 +0530

    sort.js

---
+++ sort.js
@@ -0,0 +1,23 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const url = 'mongodb://localhost:27017/myDB'
+
+MongoClient.connect(url, function (error, db) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Connect to', url);
+        let dbo = db.db('myDB');
+        let mySort = {name : 1}
+        dbo.collection("customers").find().sort(mySort).toArray( function (err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(res);
+                db.close().then(r => console.log('Connection is closed'));
+            }
+        })
+
+    }
+})
